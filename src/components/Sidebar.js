ffc8112499207406213a58f92555ad6bcace1f24
Sairaj Adarkar <sairaj.adarkar2001@gmail.com>
2024-09-05 00:18:05 +0530

    New1

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,14 +2,14 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { ChevronDownIcon } from '@heroicons/react/outline';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash, faDiceD6, faPager, faImage, faInfoCircle, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'; // Added faBars and faTimes for hamburger icon
+import { faEye, faEyeSlash, faDiceD6, faPager, faImage, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import AdminLogo from '../images/harmonieee12121.png';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useSidebar } from '../contexts/SidebarContext';
 
 const Sidebar = ({ setSelectedOption }) => {
   const { brandOpen, setBrandOpen, websiteOpen, setWebsiteOpen, illustrationOpen, setIllustrationOpen, isFirstRender } = useSidebar();
-  const [isVisible, setIsVisible] = useState(false); // Visibility state for mobile
+  const [isVisible, setIsVisible] = useState(true);
   const [isPinned, setIsPinned] = useState(true); // Default to pinned
   const [selectedOption, setSelectedOptionState] = useState('');
   const brandContentRef = useRef(null);
@@ -17,11 +17,6 @@ const Sidebar = ({ setSelectedOption }) => {
   const illustrationContentRef = useRef(null);
   const location = useLocation();
   const navigate = useNavigate();
-
-  // New function to toggle sidebar visibility for mobile
-  const toggleSidebar = () => {
-    setIsVisible(!isVisible);
-  };
 
   const togglePin = () => {
     setIsPinned(!isPinned);
@@ -77,6 +72,21 @@ const Sidebar = ({ setSelectedOption }) => {
   }, [illustrationOpen]);
 
   useEffect(() => {
+    if (!isPinned) {
+      const handleMouseEnter = () => setIsVisible(true);
+      const handleMouseLeave = () => setIsVisible(false);
+
+      document.querySelector('.sidebar').addEventListener('mouseenter', handleMouseEnter);
+      document.querySelector('.sidebar').addEventListener('mouseleave', handleMouseLeave);
+
+      return () => {
+        document.querySelector('.sidebar').removeEventListener('mouseenter', handleMouseEnter);
+        document.querySelector('.sidebar').removeEventListener('mouseleave', handleMouseLeave);
+      };
+    }
+  }, [isPinned]);
+
+  useEffect(() => {
     const currentPath = location.pathname;
     if (currentPath.includes('one-color')) {
       setSelectedOptionState('1 color');
@@ -103,18 +113,10 @@ const Sidebar = ({ setSelectedOption }) => {
 
   return (
     <>
-      {/* Mobile Navbar with Hamburger icon */}
-      <div className="md:hidden flex justify-between items-center bg-blue-900 px-4 py-2 text-white">
-        <button onClick={toggleSidebar} className="text-2xl focus:outline-none">
-          <FontAwesomeIcon icon={isVisible ? faTimes : faBars} />
-        </button>
-        <h1 className="text-lg font-bold">Harmony</h1>
-      </div>
-
-      {/* Sidebar with visibility toggle */}
       <div
-        className={`sidebar bg-white text-gray-800 h-full fixed top-0 z-30 transition-transform duration-300 ${isVisible ? 'translate-x-0' : '-translate-x-full'
-          } md:translate-x-0 md:w-64 sf-pro-font ${isPinned ? 'pinned' : ''}`}
+        className={`sidebar bg-white text-gray-800 h-full flex flex-col fixed top-0 z-30 transition-transform duration-300 ${
+          isVisible ? 'translate-x-0' : '-translate-x-2/3'
+        } sf-pro-font ${isPinned ? 'pinned' : ''}`}
         style={{ width: '16rem' }}
       >
         <div className="sidebar-header p-4 flex justify-between items-center">
@@ -127,18 +129,24 @@ const Sidebar = ({ setSelectedOption }) => {
         </div>
         <div className="p-4 flex-grow overflow-y-auto scrollable-content">
           <div
-            className={`relative mb-4 section ${selectedOption.includes('color') || selectedOption === 'Brand' ? 'bg-off-white highlighted-section' : ''
-              }`}
+            className={`relative mb-4 section ${
+              selectedOption.includes('color') || selectedOption === 'Brand' ? 'bg-off-white highlighted-section' : ''
+            }`}
           >
             <button
               onClick={handleBrandToggle}
-              className={`flex justify-between items-center w-full text-left px-4 py-2 rounded-md hover:text-black hover:opacity-100 ${selectedOption.includes('color') ? 'text-black opacity-100' : 'opacity-70'
-                }`}
+              className={`flex justify-between items-center w-full text-left px-4 py-2 rounded-md hover:text-black hover:opacity-100 ${
+                selectedOption.includes('color') ? 'text-black opacity-100' : 'opacity-70'
+              }`}
             >
               <span className={`text-lg font-bold ${selectedOption.includes('color') ? 'text-black opacity-100' : 'opacity-70'}`}>
                 Brand
               </span>
               <ChevronDownIcon className={`h-4 w-4 ${brandOpen ? 'transform rotate-180' : ''} ml-2`} />
+              <FontAwesomeIcon
+                icon={faDiceD6}
+                className={`h-5 w-5 ml-auto ${selectedOption.includes('color') ? 'text-black opacity-100' : 'opacity-70'}`}
+              />
             </button>
             <div
               ref={brandContentRef}
@@ -148,8 +156,9 @@ const Sidebar = ({ setSelectedOption }) => {
               <div className="mt-2 w-full">
                 <button
                   onClick={() => handleOptionSelect('1 color')}
-                  className={`flex justify-between items-center w-full text-left px-4 py-2 hover:text-black hover:opacity-100 ${selectedOption === '1 color' ? 'neon-text' : 'opacity-70'
-                    }`}
+                  className={`flex justify-between items-center w-full text-left px-4 py-2 hover:text-black hover:opacity-100 ${
+                    selectedOption === '1 color' ? 'neon-text' : 'opacity-70'
+                  }`}
                   style={{ marginBottom: '0.5rem' }}
                 >
                   <span className={`font-bold ${selectedOption === '1 color' ? 'neon-text' : 'opacity-70'}`}>1 Color</span>
@@ -157,7 +166,8 @@ const Sidebar = ({ setSelectedOption }) => {
                 </button>
                 <button
                   onClick={() => handleOptionSelect('2 colors')}
-                  className={`flex justify-between items-center w-full text-left px-4 py-2 hover:text-black hover:opacity-100 ${selectedOption === '2 colors' ? 'neon-text' : 'opacity-70'}`}
+                  className={`flex justify-between items-center w-full text-left px-4 py-2 hover:text-black hover:opacity-100 ${
+                    selectedOption === '2 colors' ? 'neon-text' : 'opacity-70'}`}
                   style={{ marginBottom: '0.5rem' }}
                 >
                   <span className={`font-bold ${selectedOption === '2 colors' ? 'neon-text' : 'opacity-70'}`}>2 Colors</span>
@@ -165,7 +175,8 @@ const Sidebar = ({ setSelectedOption }) => {
                 </button>
                 <button
                   onClick={() => handleOptionSelect('3 colors')}
-                  className={`flex justify-between items-center w-full text-left px-4 py-2 hover:text-black hover:opacity-100 ${selectedOption === '3 colors' ? 'neon-text' : 'opacity-70'}`}
+                  className={`flex justify-between items-center w-full text-left px-4 py-2 hover:text-black hover:opacity-100 ${
+                    selectedOption === '3 colors' ? 'neon-text' : 'opacity-70'}`}
                 >
                   <span className={`font-bold ${selectedOption === '3 colors' ? 'neon-text' : 'opacity-70'}`}>3 Colors</span>
                   <FontAwesomeIcon icon={faDiceD6} className={`h-5 w-5 ml-auto ${selectedOption === '3 colors' ? 'neon-text' : 'opacity-70'}`} />
@@ -175,12 +186,14 @@ const Sidebar = ({ setSelectedOption }) => {
           </div>
           <div className="sidebar-separator"></div> {/* Separator between sections */}
           <div
-            className={`relative mb-4 section ${selectedOption.includes('tone') || selectedOption === 'Website' ? 'bg-off-white highlighted-section' : ''
-              }`}
+            className={`relative mb-4 section ${
+              selectedOption.includes('tone') || selectedOption === 'Website' ? 'bg-off-white highlighted-section' : ''
+            }`}
           >
             <button
               onClick={handleWebsiteToggle}
-              className={`flex justify-between items-center w-full text-left px-4 py-2 rounded-md hover:text-black hover:opacity-100 ${selectedOption.includes('tone') ? 'text-black opacity-100' : 'opacity-70'}`}
+              className={`flex justify-between items-center w-full text-left px-4 py-2 rounded-md hover:text-black hover:opacity-100 ${
+                selectedOption.includes('tone') ? 'text-black opacity-100' : 'opacity-70'}`}
             >
               <span className={`text-lg font-bold ${selectedOption.includes('tone') ? 'text-black opacity-100' : 'opacity-70'}`}>
                 Website
@@ -199,7 +212,8 @@ const Sidebar = ({ setSelectedOption }) => {
               <div className="mt-2 w-full">
                 <button
                   onClick={() => handleOptionSelect('1 tone')}
-                  className={`flex justify-between items-center w-full text-left px-4 py-2 hover:text-black hover:opacity-100 ${selectedOption === '1 tone' ? 'neon-text' : 'opacity-70'}`}
+                  className={`flex justify-between items-center w-full text-left px-4 py-2 hover:text-black hover:opacity-100 ${
+                    selectedOption === '1 tone' ? 'neon-text' : 'opacity-70'}`}
                   style={{ marginBottom: '0.5rem' }}
                 >
                   <span className={`font-bold ${selectedOption === '1 tone' ? 'neon-text' : 'opacity-70'}`}>1 Tone</span>
@@ -207,7 +221,8 @@ const Sidebar = ({ setSelectedOption }) => {
                 </button>
                 <button
                   onClick={() => handleOptionSelect('2 tones')}
-                  className={`flex justify-between items-center w-full text-left px-4 py-2 hover:text-black hover:opacity-100 ${selectedOption === '2 tones' ? 'neon-text' : 'opacity-70'}`}
+                  className={`flex justify-between items-center w-full text-left px-4 py-2 hover:text-black hover:opacity-100 ${
+                    selectedOption === '2 tones' ? 'neon-text' : 'opacity-70'}`}
                 >
                   <span className={`font-bold ${selectedOption === '2 tones' ? 'neon-text' : 'opacity-70'}`}>2 Tones</span>
                   <FontAwesomeIcon icon={faPager} className={`h-5 w-5 ml-auto ${selectedOption === '2 tones' ? 'neon-text' : 'opacity-70'}`} />
@@ -215,14 +230,16 @@ const Sidebar = ({ setSelectedOption }) => {
               </div>
             </div>
           </div>
-
+          
           <div className="sidebar-separator"></div> {/* Separator below Illustration */}
           <div
-            className={`relative mb-4 section ${selectedOption === 'About' ? 'bg-off-white highlighted-section' : ''}`}
+            className={`relative mb-4 section ${
+              selectedOption === 'About' ? 'bg-off-white highlighted-section' : ''}`}
           >
             <button
               onClick={() => handleOptionSelect('About')}
-              className={`flex justify-between items-center w-full text-left px-4 py-2 rounded-md hover:text-black hover:opacity-100 ${selectedOption === 'About' ? 'text-black opacity-100' : 'opacity-70'}`}
+              className={`flex justify-between items-center w-full text-left px-4 py-2 rounded-md hover:text-black hover:opacity-100 ${
+                selectedOption === 'About' ? 'text-black opacity-100' : 'opacity-70'}`}
             >
               <span className={`text-lg font-bold ${selectedOption === 'About' ? 'text-black opacity-100' : 'opacity-70'}`}>About</span>
               <FontAwesomeIcon icon={faInfoCircle} className={`h-5 w-5 ml-auto ${selectedOption === 'About' ? 'text-black opacity-100' : 'opacity-70'}`} />
